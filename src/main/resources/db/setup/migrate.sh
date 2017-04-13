f3300a15877a5266a38d4ecd1c23a726e301be89
Cam <cam.crockett@oracle.com>
2017-04-13 15:01:06 -0600

    updating migration script

--- src/main/resources/db/setup/migrate.sh
+++ src/main/resources/db/setup/migrate.sh
@@ -6,11 +6,8 @@ result="\"status\":\"RUNNING\""
 job="dummyjob"
 
 
-# scp file
 cd src/main/resources/db/setup
-scp -i mysqlkey -o StrictHostKeyChecking=no mysql_setup.sql opc@${MYSQL_IP}:/tmp/mysql_setup.sql
-ssh -i mysqlkey -o StrictHostKeyChecking=no opc@${MYSQL_IP} 
-sudo su - oracle 
-mysql "catalog" < "/tmp/mysql_setup.sql"
+scp -i mysqlkey -o StrictHostKeyChecking=no ../migration/V001__mysql_setup.sql opc@${MYSQL_IP}:/tmp/V001__mysql_setup.sql
+ssh -i mysqlkey -o StrictHostKeyChecking=no opc@${MYSQL_IP} sudo su - oracle -c 'mysql "catalog" < "/tmp/mysql_setup.sql"'
 
 echo "..done."
